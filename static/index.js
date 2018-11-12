8f1e71078f45badd5b3a86270abea9ef033c5296
David Leigh <daleighan@gmail.com>
2018-11-12 15:05:37 -0800

    Moved the js to its own folder

---
+++ static/index.js
@@ -0,0 +1,51 @@
+axios
+  .get('/splash_resources')
+  .then(function(response) {
+    var parser = new DOMParser();
+    response.data.popularLocations.forEach(function(option) {
+      var htmlString = parser.parseFromString(
+        '\
+      <div class="card text-center"> \
+        <img class="location-image" src="assets/' +
+          option.image +
+          '"> \
+        ' +
+          option.name +
+          '\
+        <div class="pb-2">\
+          <input type="radio" name="location" value="santa-monica"> \
+        </div> \
+      </div>',
+        'text/html',
+      ).body.firstChild;
+      var parentNode = document.getElementById('popular-list');
+      parentNode.append(htmlString);
+    });
+  })
+  .catch(function(err) {
+    console.log('error: ', err);
+  });
+
+var uploadInput = document.getElementById('image-upload');
+uploadInput.addEventListener('change', function() {
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    document.getElementById('preview-image').src = e.target.result;
+    document.getElementById('upload-holder').style.display = 'none';
+  };
+  reader.readAsDataURL(uploadInput.files[0]);
+});
+
+document.addEventListener('keypress', function(e) {
+  e.keyCode === 13 && e.preventDefault();
+});
+//This will eventually need to get real amounts based demand, but for now will show something
+var enteredAmount = document.getElementById('desired-price');
+enteredAmount.addEventListener('change', function(e) {
+  console.log(e.target.value);
+});
+
+var optionCards = document.getElementById('option-cards');
+function calculateOptions() {
+  optionCards.style.visibility = 'visible';
+}
