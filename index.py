38789a855f5bb901a344f83c6fb176c74d50bcba
David Leigh <daleighan@gmail.com>
2018-11-19 14:17:33 -0800

    User images are now uploaded to the server from index.html

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, abort
+from werkzeug.utils import secure_filename
 import os
 import json
 app = Flask(__name__, static_folder='/static')
+app.config["UPLOAD_FOLDER"] = "./user_images"
 
 @app.route("/splash_resources")
 def respond():
@@ -34,6 +36,17 @@ def respond():
             }
         ]
     })
+
+@app.route("/purchase", methods=["POST"])
+def handle_cart():
+    if request.method == "POST":
+        print(request.form.get("cart"))
+    if "file" not in request.files:
+        return abort(404)
+    file = request.files.get("file")
+    filename = secure_filename(file.name)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename + ".jpg"))
+    return request.form.get("cart")
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
