ef20e3121a50b0c311c354eb1c74b7d395d0616a
David Leigh <daleighan@gmail.com>
2018-12-07 18:17:17 -0800

    The table has been added to the confirmation page

--- static/confirm.js
+++ static/confirm.js
@@ -1,4 +1,5 @@
 (function() {
+  let parser = new DOMParser();
   let cart = null;
   let sessionId = sessionStorage.getItem('sessionId');
   axios
@@ -11,6 +12,60 @@
     .then(result => {
       cart = result.data;
       console.log('cart: ', cart);
+      let html = parser.parseFromString(
+        `
+      <div>
+        <table class="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col" style="width: 20%">Item</th>
+              <th scope="col" style="width: 20%">Price</th>
+              <th scope="col" style="width: 20%">Quantity</th>
+              <th scope="col" style="width: 20%">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td scope="row">Extra Days</td>
+              <td>${(cart.pricePerDay / 100).toFixed(2)}</td>
+              <td>
+                ${cart.addedDays}
+              </td>
+              <td id="day-quantity-total">$${cart.addedDays * cart.pricePerDay}</td>
+            </tr>
+            <tr>
+              <td scope="row">Extra Minutes Per Day</td>
+              <td>${(cart.perMinutePerDay / 100).toFixed(2)}</td>
+              <td>
+                ${cart.addedMinutes}
+              </td>
+              <td id="mins-quantity-total">$${cart.addedMinutes *
+                cart.perMinutePerDay *
+                (cart.days + cart.addedDays)}</td>
+            </tr>
+            <tr>
+              <td>
+                Total minutes per day: ${cart.addedMinutes +
+                  cart.secondsPerDay / 60}
+              <td>
+                Total cost per day: ${cart.total /
+                  100 /
+                  (cart.days + cart.addedDays)}
+              </td>
+              </td>
+              <td>
+                Total days: ${cart.days + cart.addedDays}
+              </td>
+              <td>
+                Total cost: $${(cart.total / 100).toFixed(2)}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>`,
+        'text/html' 
+      ).body.firstChild;
+      document.getElementById('root').appendChild(html);
     })
-    .catch(e => (window.location = '/'));
+    .catch(e => console.log('error: ', e) /*(window.location = '/') */);
 })();
