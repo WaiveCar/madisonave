2e7a650b93eab461f68f7e0e654f9af525549bf8
David Leigh <daleighan@gmail.com>
2018-12-13 16:45:39 -0800

    Added more comments to index.py

--- index.py
+++ index.py
@@ -1,7 +1,5 @@
 from flask import Flask, send_from_directory, request, session, make_response, abort, redirect, jsonify, g
 import sqlite3
-from werkzeug.utils import secure_filename
-import sqlite3
 import os
 import json
 from modules import s3
@@ -25,10 +23,14 @@ app.config['MAIL_USE_SSL'] = True
 app.config["TESTING"] = True
 mail = Mail(app)
 
+# This variable stores the path of the sqlite database
 DATABASE = os.getcwd() + "/ad-platform.db"
-
+# This dictionary is currently used for storing session information. It should later be switched to
+# some kind of better caching system, but this works for now
 active_sessions = dict()
 
+# This route sends popular locations to the client and gives them a session id if they don't 
+# already have one. The popular locations will be replaced by something that isn't mock data
 @app.route("/splash_resources")
 def respond():
     old_session_id = request.headers.get("Session-Id")
@@ -55,6 +57,8 @@ def respond():
     response.headers.set("Session-Id", session_uuid)
     return response
 
+# This route is for fetching deals. It is currently used only for fetching 3 deals, but is designed to be 
+# easily modified to additionally be able to fetch a single deal
 @app.route("/deal")
 def get_deals():
     zone = request.args.get("zone")
@@ -71,6 +75,8 @@ def get_deals():
         end_obj = datetime.datetime.utcfromtimestamp(int(request.args.get("end")))
     quoteId = request.args.get("quoteId")
     if not is_splash:
+        # This is currently not used for anything. Eventually, it will return a single deal as 
+        # opposed to the call from the splash screen that returns 3 deals
         return jsonify({
             "id": quoteId,
             "zone": "the zone",
@@ -130,6 +136,7 @@ def get_deals():
         active_sessions[quote_id] = deal
         return jsonify(deal)
 
+# This route is used to fetch a cart by session id. It is currently used by the client's confirmation page
 @app.route("/prev_cart")
 def retrieve_cart():
     old_session_id = request.headers.get("Session-Id")
@@ -138,6 +145,8 @@ def retrieve_cart():
     else:
         return "No Previous Purchase", 400
 
+# This route is used when carts are purchased. The post request is to make the intial purchase row
+# in the table while the put request is for updating the row with the information sent back by paypal
 @app.route("/capture", methods=["POST", "PUT"])
 def handle_cart():
     if request.method == "POST":
@@ -152,18 +161,24 @@ def handle_cart():
             quote_id = request.form.get("quoteId")
             active_sessions[quote_id] = [cart]
             file = request.files.get("file")
+            #The filename for the file to upload will be a uuid
             file.filename = str(uuid4()) + ".jpg"
             # To make the file actually upload to the s3 bucket, the lines below need to be commented back in
+            # They are currently commented out so as not to waste space in it
             '''
             if file:
                 uploaded = s3.upload_s3(file)
             '''
+            # Here an entry is made in the assets table for the uploaded image. Additional info about
+            # the asset will probably need to be added in the future
             asset_query = "insert into assets (path) values ('{}');".format(file.filename)
             db_connection = sqlite3.connect(DATABASE)
             cursor = db_connection.cursor()
             cursor.execute(asset_query)
             db_connection.commit()
             asset_id = cursor.lastrowid
+            # This query is for creating the new row in the purchases table. There is probably additional
+            # relevant information to add to purchases at a later time
             purchases_query = "insert into purchases (quote_id, service, asset_id, extra_days, extra_minutes_per_day, total_price, days, price_per_day, per_minute_per_day, start_date, end_date) values ('{}', 'paypal', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(quote_id, asset_id, cart["addedDays"], cart["addedMinutes"], cart["total"], cart["days"], cart["pricePerDay"], cart["perMinutePerDay"], cart["start"], cart["end"])
             cursor.execute(purchases_query)
             db_connection.commit()
@@ -171,13 +186,15 @@ def handle_cart():
             # The object that is persisted needs to have quote id, service (currently paypal), asset id (photo),
             # order id (from paypal), user's email (from paypal), total cost, added days, added mins per day, Paid (true or false).
             # the items that are not already added in this request are added in the put request that is made after the paypal
-            # payment is completed.
+            # payment is completed
             return 'Advertising Successfully Reserved', 200
         except Exception as e:
             print(e)
             return "Error on attempt at initial capture", 400
     if request.method == "PUT":
         try:
+            # This request updates the purchase entry with the information that the client has received after
+            # the paypal purchase is completed
             quote_id = request.json["quoteId"]
             payer = json.loads(request.json["payer"])
             payment_info = json.loads(request.json["paymentInfo"])
@@ -188,7 +205,8 @@ def handle_cart():
             db_connection.commit();
             db_connection.close()
             # Once all requesite info is collected, for an advertisment, an email will also need to be sent out and
-            # the user is redirected to a page summarizing what they just ordered
+            # the user is redirected to a page summarizing what they just ordered. The cart that they ordered is stored
+            # in the chache so that it can be pulled up by the confirmation page
             cart = active_sessions[quote_id][0]
             active_sessions[quote_id][0]["email"] = payer["payer_info"]["email"]
             msg = Message("Hello", 
@@ -230,8 +248,10 @@ def handle_cart():
                         </p>
                     </div>
                 </div>"""
+            # The sending of this e-mail will likely need to be modified once the correct e-mail configuration information is added
             with mail.record_messages() as outbox:
                 mail.send(msg)
+            # If the storage of the information returned by paypal is successful, the user is sent to the confirmation page
             return jsonify({"location": "confirm.html"})
         except Exception as e:
             return "Error at capture update: {}".format(e), 400
