25cca0881bce401122cf5e56fb8d072b34b0a475
Chris McKenzie <kristopolous@yahoo.com>
2019-03-22 16:42:09 -0700

    Adding a script to set up in debian

---
+++ tools/setup-debian.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python3 python3-numpy libfreetype6-dev libpng-dev sqlite3 nodejs npm
+pip3 install --upgrade pip setuptools
+sudo pip3 install -r requirements.txt
+npm install
+sqlite3 ad-platform.db < create-tables.sql
